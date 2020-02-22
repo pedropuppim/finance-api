7813e43ba15ede779e75c0c63704fd55e88fdfd5
Pedro <ppuppim@gmail.com>
2020-02-22 20:46:16 -0300

    migrations accounts

---
+++ src/migrations/20200222202508_create_accounts.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("accounts", function(table) {
+      table.increments();
+      table.string("name").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.dateTime("updatedAt").notNullable().defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable("accounts");
+  };
+  
