a2d2a2c4d7a93da1ffbac11723f4a55e5b00045c
Pedro <ppuppim@gmail.com>
2020-02-22 21:55:45 -0300

    migrations add fild active

---
+++ src/migrations/20200222214551_alter_table_accounts.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+    return  knex.schema.alterTable('accounts', function(table) {
+        table.string('active',1).defaultTo(1);
+      });
+
+};
+
+
+exports.down = function(knex, Promise) {
+    
+    return  knex.schema.table('accounts', function (table) {
+        table.dropColumn('active');
+    });
+};
