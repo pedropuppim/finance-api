2519f5134576be839f1839fa88f4f76e43041063
Pedro <ppuppim@gmail.com>
2020-02-23 10:59:30 -0300

    bills

---
+++ src/controllers/bills.js
@@ -0,0 +1,62 @@
+const options = require("./../config/config");
+
+module.exports = app => {
+
+  const list = async (req, res) => {
+    
+    const page = req.query.page || "1";
+
+
+    const bills = await app.db('bills').where({ active: 1 })
+    .modify(function(queryBuilder) {
+      if(req.query.status) {
+          queryBuilder.where('status', '=',req.query.status )
+      }
+    })
+    .paginate({ perPage: options.paginate.recordsPerPage, page });
+    
+    return res.status(200).json(bills);
+    
+
+  };
+
+  const save = async (req, res) => {
+
+    app.db('bills')
+        .insert(req.body)
+        .then(_ => res.status(201).send())
+        .catch(err => res.status(400).json(err))
+  };
+
+  const update = (req, res) => {
+
+    app.db('bills')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(_ => res.status(200).json(req.body))
+        .catch(err => res.status(400).json(err))
+  };
+
+  const remove = (req, res) => {
+
+    
+    if (!req.params.id) {
+        return res.status(400).send('Id required')
+    } 
+
+    app.db('bills')
+        .where({ id: req.params.id, active: 1 })
+        .update({active: 0})
+        .then(rowsDeleted => {
+            if (rowsDeleted > 0) {
+                res.status(204).send()
+            } else {
+                const msg = `record not found with id ${req.params.id}.`
+                res.status(400).send(msg)
+            }
+        })
+        .catch(err => res.status(400).json(err))
+  }
+
+  return { list, save, update, remove };
+};
